242957145115f7fa049dc95626b51346b2f87156
Sameera Piyadigamage <sdpsameera@gmail.com>
2021-11-12 12:00:34 +0530

    typescript boiler plate

--- __tests__/calculator.test.ts
+++ __tests__/calculator.test.ts
@@ -1,9 +1,10 @@
-const calculator = require('../calculator');
+import { sumTwoIntegers } from "../calculator";
+import {  } from "ts-jest";
 
 describe('calculator', () => {
     describe('sumTwoIntegers(x,y)', () => {
         it('should return 40', () => {
-            const value = calculator.sumTwoIntegers(15, 25);
+            const value = sumTwoIntegers(15, 25);
             expect(value).toEqual(40);
         });
     });
--- calculator.ts
+++ calculator.ts
@@ -1,11 +1,7 @@
-const sumTwoIntegers = function (x, y) {
+export const sumTwoIntegers = function (x: number, y: number) {
     //validating the integrity of the inputs
     if (isNaN(x) || isNaN(y)) {
         throw new Error('X and Y values only can be integers');
     }
     return x + y;
-};
-
-module.exports = {
-    sumTwoIntegers: sumTwoIntegers,
 };
